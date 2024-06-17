175dc22de0d8eee2f40d35b5075656d28edd2200
nicolenaspect <nicolepavlova9@gmail.com>
2024-06-17 22:00:58 +0300

    --

---
+++ 02-SoftUni-fundamentals/exam-preparation/counter-strike.py
@@ -0,0 +1,45 @@
+def main():
+    won_battles = 0
+    battles_counter = 0
+    initial_energy = int(input())
+
+    while True:
+        enemy_distance = input()
+        if enemy_distance == "End of battle":
+            print(f"Won battles: {won_battles}. Energy left: {initial_energy}")
+            break
+        if initial_energy - int(enemy_distance) >= 0:
+            won_battles += 1
+            battles_counter += 1
+            if battles_counter == 3:
+                initial_energy += won_battles
+                battles_counter = 0
+            initial_energy -= int(enemy_distance)
+        else:
+            print(
+                f"Not enough energy! Game ends with {won_battles} won battles and {initial_energy} energy"
+            )
+            break
+
+
+main()
+
+"""   
+100
+10
+10
+10
+1
+2
+3
+73
+10
+"""
+"""
+200
+54
+14
+28
+13
+End of battle
+"""
