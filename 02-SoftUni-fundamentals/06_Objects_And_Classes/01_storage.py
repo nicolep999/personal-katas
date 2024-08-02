cf3dc3f645e368594cdb6c779c10f7b7384590ac
nicolepavlova9 <nicolepavlova9@gmail.com>
2024-08-02 17:15:09 +0300

    -

---
+++ 02-SoftUni-fundamentals/06_Objects_And_Classes/01_storage.py
@@ -0,0 +1,14 @@
+class Storage:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.storage = []
+
+    def add_product(self, product: str):
+        if len(self.storage) < self.capacity:
+            self.storage.append(product)
+
+    def get_products(self):
+        return self.storage
+
+
+storage = Storage(4)
