4082ce5db0207a68ae3e341ef5e4c3a58ac6d570
nikolp <nicolepavlova9@gmail.com>
2024-11-09 16:06:42 +0200

    -

--- 03_softuni_advanced/oop/06_polymorphism/exercise/03_account.py
+++ 03_softuni_advanced/oop/06_polymorphism/exercise/03_account.py
@@ -4,3 +4,108 @@ class Account:
         self.owner = owner
         self.amount = amount
         self._transactions: list = []
+
+    def handle_transaction(self, transaction_amount: float) -> str:
+        if self.balance + transaction_amount < 0:
+            raise ValueError("sorry cannot go in debt!")
+        self._transactions.append(transaction_amount)
+        return f"New balance: {self.balance}"
+
+    def add_transaction(self, amount: int) -> str:
+        if not isinstance(amount, int):
+            raise ValueError("please use int for amount")
+        return self.handle_transaction(amount)
+
+    @property
+    def balance(self):
+        return self.amount + sum(self._transactions)
+
+    def __str__(self):
+        return f"Account of {self.owner} with starting amount: {self.amount}"
+
+    def __repr__(self):
+        return f"Account({self.owner}, {self.amount})"
+
+    def __len__(self):
+        return len(self._transactions)
+
+    def __iter__(self):
+        return iter(self._transactions)
+
+    def __getitem__(self, index):
+        return self._transactions[index]
+
+    def __reversed__(self):
+        return reversed(self._transactions)
+
+    def __eq__(self, other):
+        return self.balance == other.balance
+
+    def __ne__(self, other):
+        return self.balance != other.balance
+
+    def __lt__(self, other):
+        return self.balance < other.balance
+
+    def __le__(self, other):
+        return self.balance <= other.balance
+
+    def __gt__(self, other):
+        return self.balance > other.balance
+
+    def __ge__(self, other):
+        return self.balance >= other.balance
+
+    def __add__(self, other):
+        new_owner = f"{self.owner}&{other.owner}"
+        new_initial_amount = self.amount + other.amount
+        new_account = Account(new_owner, new_initial_amount)
+        new_account._transactions = self._transactions + other._transactions
+        return new_account
+
+
+import unittest
+
+
+class TestsAccount(unittest.TestCase):
+    def setUp(self):
+        self.acc1 = Account("Johhny")
+        self.acc2 = Account("Anna", 40)
+
+    def test_init(self):
+        self.assertEqual(self.acc1.owner, "Johhny")
+        self.assertEqual(self.acc1.amount, 0)
+        self.assertEqual(self.acc1._transactions, [])
+        self.assertEqual(self.acc2.owner, "Anna")
+        self.assertEqual(self.acc2.amount, 40)
+        self.assertEqual(self.acc2._transactions, [])
+
+    def test_repr(self):
+        self.assertEqual(repr(self.acc1), "Account(Johhny, 0)")
+
+    def test_str(self):
+        self.assertEqual(str(self.acc2), "Account of Anna with starting amount: 40")
+
+    def test_add_transaction(self):
+        self.acc1.add_transaction(10)
+        self.assertEqual(self.acc1._transactions, [10])
+        with self.assertRaises(ValueError) as cm:
+            self.acc1.add_transaction(9.9)
+        self.assertEqual(str(cm.exception), "please use int for amount")
+
+    def test_balance(self):
+        self.acc2.add_transaction(-20)
+        self.assertEqual(self.acc2.balance, 20)
+
+    def test_len(self):
+        self.acc1.add_transaction(10)
+        self.acc1.add_transaction(-5)
+        self.assertEqual(len(self.acc1), 2)
+
+    def test_get_item(self):
+        self.acc1.add_transaction(5)
+        self.assertEqual(self.acc1[0], 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
