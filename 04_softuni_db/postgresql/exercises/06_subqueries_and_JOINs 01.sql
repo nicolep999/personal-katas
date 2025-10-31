037640b25970b76087bfa83e06b18f4ee791a28b
Nicole P. <116297912+nicolep999@users.noreply.github.com>
2025-10-31 13:44:23 +0200

    x

--- 04_softuni_db/postgresql/exercises/06_subqueries_and_JOINs 01.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    concat_ws(' ', address, address_2) as apartment_address,
-    b.booked_for as nights
-FROM apartments as a
-    JOIN bookings as b ON a.booking_id = b.booking_id
-    ORDER BY a.apartment_id;
